a2a3d1ea9b65bd0825604d54148eb79c229cfd2f
Yukti Goswami <yuktig.jaipur@gmail.com>
2021-09-17 22:44:51 +0530

    Day #3

--- ArrayKthSmalletElement.java
+++ ArrayKthSmalletElement.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class KthSmalletElement {
+public class ArrayKthSmalletElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of array:");
