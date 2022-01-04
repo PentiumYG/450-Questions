6d0ad81e898256ae6bad62ecff1511824decec5b
Yukti Goswami <yuktig.jaipur@gmail.com>
2022-01-04 15:13:37 +0530

    Day#2 P1 Added

---
+++ ArrayMinJumps.java
@@ -0,0 +1,43 @@
+
+    import java.io.*;
+  //  import java.util.*;
+    class GFG
+     {
+        public static void main (String[] args) throws IOException
+         {
+         
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+            int t = Integer.parseInt(br.readLine()); 
+    
+            while(t-- > 0){
+                int size = Integer.parseInt(br.readLine());
+                String[] arrStr = ((String)br.readLine()).split("\\s+");
+                int[] arr= new int[size];
+                for(int i = 0;i<size;i++){
+                    arr[i] = Integer.parseInt(arrStr[i]);
+                }
+                new MinJumps();
+                System.out.println(MinJumps.minJumps(arr));
+            }
+         }
+         
+    }
+    // } Driver Code Ends
+    
+    
+    class MinJumps{
+        static int minJumps(int[] arr){
+            int i=0;
+            int n= arr.length;
+           // int steps = n-1;
+            int count = 0;
+            while(i<n){
+                if(arr[i] != 0){
+                  //  steps = steps - arr[i];
+                    i = i + arr[i];
+                    count = count + 1;
+                }
+            }
+            return count;
+        }
+    }
