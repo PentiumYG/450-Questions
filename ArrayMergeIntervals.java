d54e333fbcdfa2512a9dcd2cd13945be7bc6a1c5
Yukti Goswami <yuktig.jaipur@gmail.com>
2022-01-12 00:53:39 +0530

    merge prog added

--- ArrayMergeIntervals.java
+++ ArrayMergeIntervals.java
@@ -2,22 +2,42 @@ import java.util.*;
 
 public class ArrayMergeIntervals {
     public static void main(String[] args) {
-        
+        int[][] intervals={{2,3},{5,5},{2,2},{3,4},{3,4}};
+        MergeIntervals obj = new MergeIntervals();
+        intervals = obj.merge(intervals);
+        for(int i=0;i<intervals.length;i++){
+            for(int j=0;j<2;j++){
+                System.out.print(intervals[i][j]+" ");
+            }
+        } 
     }
 }
-class Solution {
+class MergeIntervals {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
-        List<List> a = new ArrayList<>();
-        List<Integer> b = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            b.add(0,intervals[i][0]);
-            b.add(1,intervals[i][1]);
-            a.add(b);
+        if(n==1){
+            return intervals;
         }
-        for(int i=0;i<a.size();i++){
-            for(int j=0;j<a.size();j++){
-                if((int)a.get(i).get(1)>=(int)a.get(j).get(0) && (int)a.get(i).get(0)<=(int)a.get(j).get(1)){
+        int t=0;
+        List<List> temp1 = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            List<Integer> temp2=new ArrayList<>();
+            temp2.add(intervals[i][0]);
+            temp2.add(intervals[i][1]);
+            temp1.add(temp2);
+        }
+        List<List> a = new ArrayList<>();
+        
+        for(int i=0;i<n-1;i++){
+            if(intervals[i][0]==-1){
+                continue;
+            }
+            for(int j=i+1;j<n;j++){
+                if(intervals[j][0]==-1){
+                    continue;
+                }
+                if(intervals[i][1]>=intervals[j][0] && intervals[i][0]<=intervals[j][1]){
+                    List<Integer> b = new ArrayList<>(2);
                     if(intervals[i][0]<intervals[j][0]){
                         b.add(0,intervals[i][0]);
                     }
@@ -30,13 +50,40 @@ class Solution {
                     else{
                        b.add(1,intervals[j][1]);                         
                     }
-                    a.remove(i);
-                    a.remove(j);
-                    a.add(i, b);
-
+                    a.add(b);
+                    intervals[i][0]=-1;
+                    intervals[i][1]=-1;
+                    intervals[j][0]=-1;
+                    intervals[j][1]=-1;
+                    temp1.remove(i);
+                    temp1.remove(j-1);
+                    t=1;
+                    break;
                 }
+                else{
+                    continue;
+                }   
             }
-            
+            if(t==1){
+                break;
+            }
+        }
+        a.addAll(temp1);
+        
+        int l=a.size();
+        int m=a.get(0).size();
+        int c[][]=new int[l][m];
+        for(int i=0;i<l;i++){
+            for(int j=0;j<m;j++){
+                c[i][j]=(int) a.get(i).get(j);
+            }
+        }
+        if(t==0){
+            return c;
+        }
+        else{
+            intervals = merge(c);
+            return intervals;
         }
     }
 }
--- Practice.java
+++ Practice.java
@@ -2,17 +2,43 @@ import java.util.*;
 
 public class Practice{
     public static void main(String args[]){
-        int[] arr={1, 2, 3, 4, 5, 6, 7, 8};
-        List<List> a = new ArrayList<>();
-        List<Integer> b = new ArrayList<>();
-        for(int i=1;i<8;i++){
-            b.add(0, arr[i-1]);
-            b.add(1, arr[i]);
-            i=i+1;
-            a.add(b); 
-            b.clear();
+        int[][] arr={{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+        int n=arr.length;
+        List<List> temp1 = new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            List<Integer> temp2=new ArrayList<>(2);
+            //temp2.clear();
+            temp2.add(arr[i][0]);
+            temp2.add(arr[i][1]);
+            temp1.add(temp2);
         }
 
+        System.out.println(temp1);
+
+        temp1.remove(1);
+
+        System.out.println(temp1);
+        
+        int[][] barr = new int[arr.length][2];
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<2;j++){
+                if(arr[i][j]>4){
+                    barr[i][j]=arr[i][j];
+                } 
+            }
+        } 
+        
+        List<List> a = new ArrayList<>();
+        // List<Integer> b = new ArrayList<>();
+        // for(int i=1;i<8;i++){
+        //     b.add(0, arr[i-1]);
+        //     b.add(1, arr[i]);
+        //     i=i+1;
+        //     a.add(b); 
+        //     b.clear();
+        // }
+        a.add(temp1.get(1));
         // b.add(0, arr[0]);
         // b.add(1, arr[1]);
         // a.add(b);
@@ -24,10 +50,21 @@ public class Practice{
         // b.add(0,8);
         // b.add(1,9);
         // a.add(b);
-        System.out.println(a);
-        System.out.println(b);
-        //int c=(int) a.get(0).get(0);
-        a.remove(1);
-        System.out.println(a);
+        // System.out.println(a);
+        // System.out.println(b);
+        // //int c=(int) a.get(0).get(0);
+        // a.remove(1);
+        // System.out.println(a);
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<2;j++){
+                System.out.print(arr[i][j]+" "); 
+            }
+        }
+        System.out.println();
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<2;j++){
+                System.out.print(barr[i][j]+" "); 
+            }
+        }
     }
 }
