a2d94b552fee3c8858d68aa652be9fa45fd4f9dd
Yukti Goswami <yuktig.jaipur@gmail.com>
2022-01-17 23:19:08 +0530

    Create MatrixSpiral.java

---
+++ MatrixSpiral.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class MatrixSpiral {
+    public static void main(String[] args) {
+        
+    }
+}
+class MatrixS {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int i=0, j=n, k=m, l=1;
+        List<Integer> arr=new ArrayList<>();
+        while(k==0 || k==n-i){
+            while(j==n){
+                
+            }
+        }
+        
+    }
+}
